8b4a712be79cb85ff4c00657bac10ab23a0d2792
mwangidennis1 <mwangedennis05@gmail.com>
2024-11-11 12:22:43 +0300

    add terminal and flask

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-python lambda_calculus_interpreter.py 
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install flask flask-cors
+
+pip freeze >  requirements.txt
+
+echo  "We are Fucking live! Virtual environment is activated"
+echo  "To deactivate , run: deactivate "
 
